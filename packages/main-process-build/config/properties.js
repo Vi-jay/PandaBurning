12379cda59157ab821b1840fc9b7f5bb11e850af
VijayAdam <765139628@qq.com>
2022-11-04 20:38:44 +0800

    update

--- packages/main-process-build/config/properties.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UPLOAD_URL = exports.TOMATO__SEC = exports.IS_DEV = void 0;
-exports.IS_DEV = process.env.NODE_ENV === 'development';
-exports.TOMATO__SEC = 60 * 40;
-exports.UPLOAD_URL = 'https://h5.fulugame.cn/test/download';
