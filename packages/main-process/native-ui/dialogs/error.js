35a966aace5957bb15ccb49b94d9ed8bc5ae1671
VijayAdam <765139628@qq.com>
2022-11-04 20:13:44 +0800

    update

--- packages/main-process/native-ui/dialogs/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.ipcMain.on('open-error-dialog', (event) => {
-    electron_1.dialog.showErrorBox('An Error Message', 'Demonstrating an error message.');
-});
